08dfe6f910d2f645abe8a3e1014ffad9af7bb50c
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-07 18:51:43 +0300

    Initial commit

---
+++ app/src/main/java/com/makhmutov/internshipvkmarket/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.makhmutov.internshipvkmarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.makhmutov.internshipvkmarket.ui.theme.InternshipVKMarketTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InternshipVKMarketTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
