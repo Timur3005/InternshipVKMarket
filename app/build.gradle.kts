1d255da4c9f068c78bfdc3b7c636189bfd4f8b75
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-09 12:41:58 +0300

    jetpack navigation dependency was added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
 
     implementation("io.coil-kt:coil-compose:2.6.0")
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
