0b7a58b0ec166bbfd73324796cc185ba999e44f9
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-10 14:44:22 +0300

    old colors were deleted

--- app/src/main/java/com/makhmutov/internshipvkmarket/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/makhmutov/internshipvkmarket/presentation/ui/theme/Color.kt
@@ -2,14 +2,6 @@ package com.makhmutov.internshipvkmarket.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val Blue100 = Color(0xFF006874)
 val Blue200 = Color(0xFF4fd8eb)
 val Blue300 = Color(0xFF00363d)
