83efa1188ccbd6f0c73d783082251913f0b3389e
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-11 11:27:53 +0300

    edge to edge mode was enabled

--- app/src/main/java/com/makhmutov/internshipvkmarket/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/makhmutov/internshipvkmarket/presentation/activity/MainActivity.kt
@@ -2,7 +2,9 @@ package com.makhmutov.internshipvkmarket.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +16,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT
+            )
+        )
         setContent {
             InternshipVKMarketTheme {
                 Surface(
