df76bb20aac278ee2a6ce66a96821a49e4b04530
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-11 11:42:53 +0300

    ui into ProductsScreen.kt was improved

--- app/src/main/java/com/makhmutov/internshipvkmarket/presentation/screens/products/ProductsScreen.kt
+++ app/src/main/java/com/makhmutov/internshipvkmarket/presentation/screens/products/ProductsScreen.kt
@@ -1,13 +1,16 @@
 package com.makhmutov.internshipvkmarket.presentation.screens.products
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -27,12 +30,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.SubcomposeAsyncImage
 import com.makhmutov.internshipvkmarket.R
 import com.makhmutov.internshipvkmarket.domain.entities.MarketItemEntity
 import com.makhmutov.internshipvkmarket.presentation.app.getApplicationComponent
+import java.nio.file.WatchEvent
 
 @Composable
 fun ProductsScreen(
@@ -203,7 +208,8 @@ private fun ProductCard(
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
-            Text(text = product.title, style = MaterialTheme.typography.titleLarge)
+            Text(text = product.title, style = MaterialTheme.typography.titleLarge
+                .copy(fontWeight = FontWeight.Bold))
             Spacer(modifier = Modifier.height(8.dp))
             SubcomposeAsyncImage(
                 model = product.thumbnail,
@@ -232,6 +238,24 @@ private fun ProductCard(
                     }
                 }
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ){
+                Text(
+                    text = "Cost ${product.price}$",
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+                )
+                if (product.discountPercentage > 0.0){
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "SALE -${product.discountPercentage}%",
+                        style = MaterialTheme.typography.bodyLarge
+                            .copy(fontWeight = FontWeight.Bold)
+                    )
+                }
+            }
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = product.description, style = MaterialTheme.typography.bodyMedium)
         }
