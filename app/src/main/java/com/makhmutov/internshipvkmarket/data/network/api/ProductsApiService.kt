cb365a841ad95e421cdef17806bd2aaca1d77e74
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-11 12:42:09 +0300

    methods have been added to the Api Service to implement sorting by category

--- app/src/main/java/com/makhmutov/internshipvkmarket/data/network/api/ProductsApiService.kt
+++ app/src/main/java/com/makhmutov/internshipvkmarket/data/network/api/ProductsApiService.kt
@@ -2,6 +2,8 @@ package com.makhmutov.internshipvkmarket.data.network.api
 
 import com.makhmutov.internshipvkmarket.data.network.models.RequestMarketItemsContainer
 import retrofit2.http.GET
+import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ProductsApiService {
@@ -12,8 +14,18 @@ interface ProductsApiService {
         @Query("skip") skip: Int = DEFAULT_SKIP_VALUE,
     ): RequestMarketItemsContainer
 
+    @GET(CATEGORIES_REQUEST)
+    suspend fun getCategories(): List<String>
+
+    @GET("$MARKET_ITEMS_BY_CATEGORY_REQUEST{category}")
+    suspend fun getMarketItemsByCategory(
+        @Path("category") category: String,
+    ): RequestMarketItemsContainer
+
     private companion object {
         const val MARKET_ITEMS_REQUEST = "/products"
+        const val CATEGORIES_REQUEST = "/products/categories"
+        const val MARKET_ITEMS_BY_CATEGORY_REQUEST = "/products/category/"
         const val LIMIT_MARKET_ITEMS_REQUEST = 20
         const val DEFAULT_SKIP_VALUE = 0
     }
